7c9ba630d5be2825eaf21dea488c01b152b26d88
bszekely@nevada.unr.edu <bszek213>
2023-02-18 20:11:29 -0800

    ADD: working on sample Entropy calculation

--- entropy.cpp
+++ entropy.cpp
@@ -4,9 +4,15 @@
 #include <cmath>
 // compile with the following command input: "g++ -fPIC -shared -o libEntropy.so entropy.cpp -std=c++17"
 double shannonEntropy(const std::vector<double>& data){
+    //Round all data to the tenths place - change this depending on input data
+    std::vector<double> dataRound;
+    for (double x : data) {
+        double rounded = std::round(x * 10.0) / 10.0;
+        dataRound.push_back(rounded);
+    }
     //defining an unordered map that will contain a value along with how many times that value appeared 
     std::unordered_map<double,int> freqMap;
-    for (double x: data){
+    for (double x: dataRound){
         freqMap[x]++;
     }
     int n = data.size(); //get size of input data
@@ -26,8 +32,89 @@ double approximateEntropy(const std::vector<double>& data, int m, float r){
     return 0;
 }
 
-double sampleEntropy(const std::vector<double>& data,int m, float r){
-    return 0;
+double calculatMean(const std::vector<double>& data) {
+    double sum = 0;
+    for (double x : data) {
+        sum += x;
+    }
+    return sum / data.size();
+}
+
+double StandardDeviation(const std::vector<double>& data) {
+    double mean = calculatMean(data);
+    double sum_of_squares = 0;
+    for (double x : data) {
+        double deviation = x - mean;
+        sum_of_squares += deviation * deviation;
+    }
+    return std::sqrt(sum_of_squares / (data.size() - 1));
+}
+double SampleEntropy_old(const std::vector<double>& data, int m, double r)
+{
+    int N = data.size();
+    int Cm = 0, Cm1 = 0;
+    double err = 0.0, sum = 0.0;
+    double sd = StandardDeviation(data);
+    err = sd * r;
+    
+    for (unsigned int i = 0; i < N - (m + 1) + 1; i++) {
+        for (unsigned int j = i + 1; j < N - (m + 1) + 1; j++) {      
+        bool eq = true;
+        //m - length series
+        for (unsigned int k = 0; k < m; k++) {
+            if (std::abs(data[i+k] - data[j+k]) > err) {
+            eq = false;
+            break;
+            }
+        }
+        if (eq) Cm++;
+        
+        //m+1 - length series
+        int k = m;
+        if (eq && std::abs(data[i+k] - data[j+k]) <= err)
+            Cm1++;
+        }
+    }
+    
+    if (Cm > 0 && Cm1 > 0)
+        return std::log((double)Cm / (double)Cm1);
+    else
+        return 0.0; 
+  
+}
+
+double sampleEntropy(const std::vector<double>& data, int m, double r){
+    //multiply r by the std
+    double sd = StandardDeviation(data);
+    double err = sd * r;
+    int n = data.size();
+    std::vector<double> last(m), curr(m);
+    int count1 = 0, count2 = 0;
+
+    for (int i = 0; i < n - m; i++) {
+        for (int j = 0; j < m; j++) {
+            last[j] = data[i + j];
+            curr[j] = data[i + j + 1];
+        }
+
+        int matches1 = 0, matches2 = 0;
+        for (int j = 0; j < m; j++) {
+            for (int k = j + 1; k < m; k++) {
+                if (std::abs(last[j] - last[k]) < err) {
+                    matches1++;
+                }
+                if (std::abs(curr[j] - curr[k]) < err) {
+                    matches2++;
+                }
+            }
+        }
+        count1 += matches1;
+        count2 += matches2;
+    }
+
+    double e1 = std::log(static_cast<double>(count1) / (n - m));
+    double e2 = std::log(static_cast<double>(count2) / (n - m - 1));
+    return e1 - e2;
 }
 
 double permEntropy(const std::vector<double>& data){
@@ -40,5 +127,10 @@ extern "C"
     {
         std::vector<double> data_vector(data, data + data_length);
         return shannonEntropy(data_vector);
+    }
+    double calculate_sample_entropy(double* data, int data_length)
+    {
+        std::vector<double> data_vector(data, data + data_length);
+        return SampleEntropy_old(data_vector,4,0.25);
     }
 }
--- entropy_python.py
+++ entropy_python.py
@@ -1,18 +1,87 @@
-#Python code that calls c++ shared object files
+"""
+Python code that calls c++ shared object files
+Postrual control entropy analsysis with Santos Dataset
+100 Hz and COP data in m
+"""
 import ctypes
 from pandas import read_csv
-# import numpy as np
-# Load the shared library
-shannon_entropy_lib = ctypes.CDLL('./libEntropy.so')
-# Declare the C function
-shannon_entropy_func = shannon_entropy_lib.calculate_shannon_entropy 
-# shannon_entropy_func.argtypes = [np.ctypeslib.ndpointer(dtype=np.double), ctypes.c_int]
-shannon_entropy_func.restype = ctypes.c_double
-#Load in data
-df = read_csv('test_cop.csv')
-# Convert the input to C array
-data = df['COPx[cm]'].to_list()
-data_array = (ctypes.c_double * len(data))(*data)
-# Call the C function
-result = shannon_entropy_func(data_array, len(data))
-print(result)
+import wfdb
+import matplotlib.pyplot as plt
+from pandas import DataFrame, read_csv
+
+class fastEntropy():
+    def __init__(self):
+        print('Initialize fastEntropy class and call libEntropy')
+        # Load the shared library
+        shannon_entropy_lib = ctypes.CDLL('./libEntropy.so')
+        # Declare the C function
+        self.shannon_entropy_func = shannon_entropy_lib.calculate_shannon_entropy 
+        self.sample_entropy_func = shannon_entropy_lib.calculate_sample_entropy
+        self.shannon_entropy_func.restype = ctypes.c_double
+        self.sample_entropy_func.restype = ctypes.c_double
+    def get_bds_info(self,path="/home/brianszekely/Desktop/ProjectsResearch/entropy_c++_python/data/BDSinfo.txt"):
+        return read_csv(path, sep='\t', header=0)
+    def extract_trials(self,df):
+        """
+        1. Extract trials that are open and closed eyes static two feet postural control
+        2. Extract trials that have the same show type - minimal
+        3. No disease 
+        4. Healthy young and Healthy old
+        """
+        pass
+    def get_data_participants(self,name="BDS00002"):
+        record = wfdb.rdrecord(name, pn_dir='hbedb/1.0.0/')
+        # record = wfdb.rdrecord('BDS00001', pn_dir='hbedb/1.0.0/')
+        # signal = record.p_signal
+        copX, copY = [], []
+        for i in range(len(record.p_signal)):
+            copX.append(record.p_signal[i][6]) #COPx
+            copY.append(record.p_signal[i][7]) #COPy
+        self.cop = DataFrame({"copX":copX,"copY":copY})
+        # Mean Offset to orient the data around 0
+        self.cop["copX"] = self.cop["copX"] - self.cop["copX"].mean()
+        self.cop["copY"] = self.cop["copY"] - self.cop["copY"].mean()
+        # # Put data in cm
+        self.cop["copX"] = self.cop["copX"] * 100
+        self.cop["copY"] = self.cop["copY"] * 100
+    def shannon_ent(self):
+        datax = self.cop['copX'].to_list()
+        datay = self.cop['copY'].to_list()
+        #Convert the input to C array
+        data_array_x = (ctypes.c_double * len(datax))(*datax)
+        data_array_y = (ctypes.c_double * len(datay))(*datay)
+        self.sh_ent_x = self.shannon_entropy_func(data_array_x, len(datax))
+        self.sh_ent_y = self.shannon_entropy_func(data_array_y, len(datay))
+        print(self.sh_ent_x)
+        print(self.sh_ent_y)
+    def sample_entropy(self):
+        datax = self.cop['copX'].to_list()
+        datay = self.cop['copY'].to_list()
+        #Convert the input to C array
+        data_array_x = (ctypes.c_double * len(datax))(*datax)
+        data_array_y = (ctypes.c_double * len(datay))(*datay)
+        self.samp_en_x = self.sample_entropy_func(data_array_x, len(datax))
+        self.samp_en_y = self.sample_entropy_func(data_array_y, len(datay))
+        print(self.samp_en_x)
+        print(self.samp_en_y)
+    def plot_cop(self):
+        fig, ax = plt.subplots(nrows=3, ncols=1, figsize=(8, 10))
+        ax[0].plot(self.cop['copX'],self.cop['copY'])
+        ax[0].set_xlabel('copX')
+        ax[0].set_ylabel('copY')
+        ax[1].plot(self.cop.index,self.cop['copX'])
+        ax[1].set_xlabel('Samples')
+        ax[1].set_ylabel('copX')
+        ax[2].plot(self.cop.index,self.cop['copY'])
+        ax[2].set_xlabel('Samples')
+        ax[2].set_ylabel('copY')
+        plt.show()
+
+def main():
+    fast = fastEntropy()
+    fast.get_data_participants()
+    fast.shannon_ent()
+    fast.sample_entropy()
+    fast.plot_cop()
+if __name__ == "__main__":
+    main()
